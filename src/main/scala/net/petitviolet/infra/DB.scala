71ddd3b99316bbb530dceb0e2122bcf209feb8de
petitviolet <violethero0820@gmail.com>
2016-03-31 22:49:18 +0900

    enable using MySQL DB through Slick

---
+++ src/main/scala/net/petitviolet/infra/DB.scala
@@ -0,0 +1,19 @@
+package net.petitviolet.infra
+
+import slick.jdbc.JdbcBackend
+import slick.jdbc.JdbcBackend.Database
+
+
+trait UsesDB {
+  val db: JdbcBackend.DatabaseDef
+}
+
+trait MixInDB {
+  val db: JdbcBackend.DatabaseDef = MixInDB.db
+}
+
+// 同じオブジェクトを参照するようにするため
+private object MixInDB {
+  val db: JdbcBackend.DatabaseDef = Database.forConfig("mysql-local")
+}
+
