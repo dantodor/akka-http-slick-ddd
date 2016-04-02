52e516e16b36731fb4895e71161a84b65573e2ab
petitviolet <violethero0820@gmail.com>
2016-04-02 11:09:42 +0900

    Repository of DDD

---
+++ src/main/scala/net/petitviolet/domain/lifecycle/Repository.scala
@@ -0,0 +1,16 @@
+package net.petitviolet.domain.lifecycle
+
+import net.petitviolet.domain.support.{Entity, Identifier}
+
+import scala.concurrent.{Future, ExecutionContext}
+
+trait Repository[Id <: Identifier[_], E <: Entity[Id]] {
+
+  def store(entity: E)(implicit ec: ExecutionContext): Future[Boolean]
+
+  def resolveBy(id: Id)(implicit ec: ExecutionContext): Future[E]
+
+  def existsBy(id: Id)(implicit ec: ExecutionContext): Future[Boolean]
+
+  def deleteBy(id: Id)(implicit ec: ExecutionContext): Future[Boolean]
+}
