39228ae220552fe055f328f0f0a4b372670affdf
petitviolet <violethero0820@gmail.com>
2016-04-02 22:47:55 +0900

    json-format for ID

--- src/main/scala/net/petitviolet/domain/support/ID.scala
+++ src/main/scala/net/petitviolet/domain/support/ID.scala
@@ -1,5 +1,19 @@
 package net.petitviolet.domain.support
 
 import net.petitviolet.domain.support.Identifier.IdType
+import spray.json._
 
-case class ID[A](value: IdType) extends AnyVal with Identifier[IdType]
+case class ID[A <: Entity[_]](value: IdType) extends AnyVal with Identifier[IdType]
+
+object ID {
+  //  implicit val idFormat = jsonFormat1(ID.apply)
+  implicit val idFormat = new RootJsonFormat[ID[_]] {
+    override def read(json: JsValue): ID[_] =
+      json.asJsObject.getFields("id") match {
+        case Seq(JsString(id)) => ID(id)
+        case _ => throw new DeserializationException("ID")
+      }
+
+    override def write(obj: ID[_]): JsValue = JsObject("id" -> JsString(obj.value))
+  }
+}
