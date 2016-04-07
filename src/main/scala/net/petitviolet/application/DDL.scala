6849d0e5a84b780ba8df48fc633622242a45ae29
petitviolet <violethero0820@gmail.com>
2016-04-07 22:35:05 +0900

    add DDL to init Database and Table

---
+++ src/main/scala/net/petitviolet/application/DDL.scala
@@ -0,0 +1,30 @@
+package net.petitviolet.application
+
+import net.petitviolet.infra.MixInDB
+import net.petitviolet.infra.user.{ Hobbies, Users }
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+object DDL extends App with MixInDB {
+  import slick.driver.MySQLDriver.api._
+  val schema =
+    Users.schema ++
+      Hobbies.schema
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+  //  val drop = db.run(schema.drop)
+  val dropDB = sqlu"DROP DATABASE `akka-db`"
+  val createDB = sqlu"CREATE DATABASE `akka-db`"
+  val useDB = sqlu"USE `akka-db`"
+
+  schema.createStatements.foreach(println)
+
+  val f = db.run(DBIO.seq(
+    dropDB,
+    createDB,
+    useDB,
+    schema.create
+  ))
+  println(Await.result(f, Duration.Inf))
+}
