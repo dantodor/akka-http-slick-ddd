692dd6bec30ab1145ea35ae1fd53a2caee263863
petitviolet <violethero0820@gmail.com>
2016-04-21 23:07:00 +0900

    expand JsonProtocol of ID for ID[A]

--- src/main/scala/net/petitviolet/domain/support/ID.scala
+++ src/main/scala/net/petitviolet/domain/support/ID.scala
@@ -5,15 +5,15 @@ import spray.json._
 
 case class ID[A <: Entity[_]](value: IdType) extends AnyVal with Identifier[IdType]
 
-object ID {
-  //  implicit val idFormat = jsonFormat1(ID.apply)
-  implicit val idFormat = new RootJsonFormat[ID[_]] {
-    override def read(json: JsValue): ID[_] =
+object ID extends DefaultJsonProtocol {
+  //  implicit def idFormat[A <: Entity[_]] = jsonFormat(ID.apply[A] _, "value")
+  implicit def idFormat[A <: Entity[_]] = new RootJsonFormat[ID[A]] {
+    override def read(json: JsValue): ID[A] =
       json.asJsObject.getFields("id") match {
         case Seq(JsString(id)) => ID(id)
         case _ => throw new DeserializationException("ID")
       }
 
-    override def write(obj: ID[_]): JsValue = JsObject("id" -> JsString(obj.value))
+    override def write(obj: ID[A]): JsValue = JsObject("id" -> JsString(obj.value))
   }
 }
