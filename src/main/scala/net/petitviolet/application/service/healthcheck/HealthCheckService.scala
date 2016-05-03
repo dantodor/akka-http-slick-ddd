d32397684c90f917f0afbb646072a5e828e390e4
petitviolet <violethero0820@gmail.com>
2016-05-03 17:25:14 +0900

    let it explicitly, routing DSL

--- src/main/scala/net/petitviolet/application/service/healthcheck/HealthCheckService.scala
+++ src/main/scala/net/petitviolet/application/service/healthcheck/HealthCheckService.scala
@@ -1,13 +1,27 @@
 package net.petitviolet.application.service.healthcheck
 
+import java.lang.management.ManagementFactory
+
 import akka.event.Logging
-import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.coding._
+import akka.http.scaladsl.marshalling.{ PredefinedToEntityMarshallers, ToResponseMarshallable }
+import akka.http.scaladsl.model.{ HttpMethods, StatusCodes }
+import akka.http.scaladsl.model.headers._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server._
+import akka.http.scaladsl.server.ExceptionHandler._
+import akka.http.scaladsl.server.directives.{ MethodDirectives, PathDirectives }
+import akka.http.scaladsl.server.util.ApplyConverter
 import net.petitviolet.application.service.ServiceBase
-import net.petitviolet.domain.pong.UsesPongService
+import net.petitviolet.domain.health.Status
+import net.petitviolet.domain.pong.{ MixInPongService, UsesPongService, Pong }
+
+import scala.concurrent.Future
+import scala.concurrent.duration._
+import scala.util.{ Try, Failure, Success }
 
 trait HealthCheckService extends ServiceBase with UsesPongService {
+
   val rejectionHandler =
     RejectionHandler
       .newBuilder()
@@ -24,37 +38,122 @@ trait HealthCheckService extends ServiceBase with UsesPongService {
     case t: Throwable => complete((StatusCodes.InternalServerError, s"Error is $t"))
   }
 
-  private val _pingRoute =
-    pathPrefix("ping") {
-      pathEnd {
-        get {
-          logRequest("/ping", Logging.InfoLevel) {
-            pongService.response()
-          }
-        }
-      } ~
-        path(".+".r) { msg =>
-          if (msg == "hoge") {
-            throw new IllegalArgumentException("cannot understand `hoge`")
-          } else if (msg.length < 10) {
-            reject {
-              new ValidationRejection(
-                "Your message is too short!",
-                Some(new IllegalArgumentException("length must be longer than 10"))
-              )
-            }
-          } else {
-            complete(s"$msg")
-          }
-        }
-    }
+  final class MyHeader(token: String) extends ModeledCustomHeader[MyHeader] {
+    override def companion: ModeledCustomHeaderCompanion[MyHeader] = MyHeader
 
-  val pingRoute =
-    handleExceptions(exceptionHandler) {
-      handleRejections(rejectionHandler) {
-        _pingRoute
+    override def value(): String = s"value: $token"
+  }
+
+  object MyHeader extends ModeledCustomHeaderCompanion[MyHeader] {
+    override def name: String = "My-Header"
+
+    // class名と違う
+    override def parse(value: String): Try[MyHeader] = Try(new MyHeader(value))
+  }
+
+  val myHeaderExtract = optionalHeaderValuePF {
+    case h @ MyHeader(token) => h
+  }
+
+  import spray.json.DefaultJsonProtocol._
+
+  case class Message(value: String)
+  implicit val format = jsonFormat1(Message.apply)
+
+  case class Num(value: Long)
+
+  // path
+  val pingSegment: String = "ping"
+  val pingMatcher: PathMatcher[Unit] = segmentStringToPathMatcher(pingSegment)
+  val pingDirective: Directive[Unit] = path(pingMatcher)
+
+  // response
+  val pongStr: String = "pong"
+  val pongMarshal: ToResponseMarshallable =
+    ToResponseMarshallable.apply(pongStr)(PredefinedToEntityMarshallers.StringMarshaller)
+  val pongStandardRoute: StandardRoute = complete(pongMarshal)
+
+  // construct route
+  val getPingPath: (StandardRoute) => Route = s =>
+    Directive.addByNameNullaryApply(MethodDirectives.get).apply(s)
+  val pingRoute: Route = getPingPath(pongStandardRoute)
+  val pingRouter: (=> Route) => Route = Directive.addByNameNullaryApply(pingDirective)
+  val _route: Route = pingRouter(pingRoute)
+
+  Directive.addByNameNullaryApply(path(segmentStringToPathMatcher("ping"))) {
+    Directive.addByNameNullaryApply(get) {
+      complete(ToResponseMarshallable("pong"))
+    }
+  }
+
+  val route =
+    path("a|b|c(hoge|foo|bar)".r) { rgx: String =>
+      get {
+        complete(s"matched: $rgx")
       }
     }
+
+  //  val route =
+  //    pathPrefix("ping") {
+  //      path("hi") {
+  //        get {
+  //          complete("hi")
+  //        }
+  //      } ~
+  //        path("a|b|c(hoge|foo|bar)+".r) { rgx: String =>
+  //          get {
+  //            complete(s"matched: $rgx")
+  //          }
+  //        } ~
+  //        path(HexIntNumber) { i =>
+  //          get {
+  //            complete(s"int: $i")
+  //          }
+  //        } ~
+  //        path(LongNumber) { l =>
+  //          get {
+  //            complete(s"long: $l")
+  //          }
+  //        } ~
+  //        path(DoubleNumber) { d =>
+  //          get {
+  //            complete(s"double: $d")
+  //          }
+  //        } ~
+  //        path(IntNumber) { hi =>
+  //          get {
+  //            complete(s"hexInt: $hi")
+  //          }
+  //        } ~
+  //        path(HexLongNumber) { hl =>
+  //          get {
+  //            complete(s"hexLong: $hl")
+  //          }
+  //        } ~
+  //        path("pong") {
+  //          pathEndOrSingleSlash {
+  //            complete(s"pong")
+  //          } ~
+  //            pathSuffix("hello") {
+  //              complete(s"hello")
+  //            }
+  //        }
+  //
+  //    } ~
+  //      post {
+  //        logRequestResult("") {
+  //          path("pong") {
+  //            complete("nice")
+  //          }
+  //        }
+  //      }
+
+  //  val pingRoute =
+  //    handleExceptions(exceptionHandler) {
+  //      handleRejections(rejectionHandler) {
+  //        _pingRoute
+  //      }
+  //    }
 
   private val uptimeRoute = path("uptime") {
     get {
@@ -65,6 +164,6 @@ trait HealthCheckService extends ServiceBase with UsesPongService {
     }
   }
 
-  val healthRoutes: Route = pingRoute ~ uptimeRoute
+  val healthRoutes: Route = route ~ uptimeRoute
 }
 
